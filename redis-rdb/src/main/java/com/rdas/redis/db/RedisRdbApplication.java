217e23cc128bde11bc65a1e9250fd95abf32cb67
Rana Das <rana.pratap.das@gmail.com>
2023-08-21 20:44:01 +0100

    first poc commit

---
+++ redis-rdb/src/main/java/com/rdas/redis/db/RedisRdbApplication.java
@@ -0,0 +1,13 @@
+package com.rdas.redis.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisRdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisRdbApplication.class, args);
+	}
+
+}
---
+++ redis-rdb/src/test/java/com/rdas/redis/db/RedisRdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdas.redis.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisRdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
